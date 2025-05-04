357d2f71c2b386a3dc5eec31bae2ab045411f208
顏于斌 <yubin@yanyubindeMacBook-Pro.local>
2025-05-04 22:18:30 +0800

    Delete the staff

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,46 @@
+# users/forms.py
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from .models import CustomerProfile, StaffProfile
 
 User = get_user_model()
 
-class SignUpForm(UserCreationForm):
+class CustomerSignUpForm(forms.ModelForm):
+    password = forms.CharField(label="密碼", widget=forms.PasswordInput)
     class Meta:
         model = User
         fields = ['email', 'username', 'phone']
-        widgets = {
-            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
-            'username': forms.TextInput(attrs={'placeholder': 'Username'}),
-            'phone': forms.TextInput(attrs={'placeholder': 'Phone'}),
-        }
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        user.role = User.Role.CUSTOMER
+        if commit:
+            user.save()
+            CustomerProfile.objects.create(user=user)
+        return user
+
+
+class StaffSignUpForm(forms.ModelForm):
+    password    = forms.CharField(label="密碼", widget=forms.PasswordInput)
+    invite_code = forms.CharField(label="邀請碼")
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'phone', 'invite_code']
+
+    def clean_invite_code(self):
+        code = self.cleaned_data['invite_code']
+        if not Invitation.objects.filter(code=code, valid=True).exists():
+            raise forms.ValidationError("邀請碼無效")
+        return code
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        user.role = User.Role.STAFF
+        if commit:
+            user.save()
+            StaffProfile.objects.create(user=user)
+            # 將邀請碼標記使用過
+            Invitation.objects.filter(code=self.cleaned_data['invite_code']).update(valid=False)
+        return user
--- users/views.py
+++ users/views.py
@@ -2,43 +2,50 @@
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth import views as auth_views
-from .forms import SignUpForm
+from .forms import CustomerSignUpForm, StaffSignUpForm
 
-# 註冊
-class SignUpView(generic.CreateView):
-    form_class    = SignUpForm
-    template_name = 'users/signup.html'
+# 顧客註冊
+class CustomerSignUpView(generic.CreateView):
+    form_class    = CustomerSignUpForm
+    template_name = 'users/account/signup_customer.html'
     success_url   = reverse_lazy('login')
+
+# 員工註冊
+class StaffSignUpView(generic.CreateView):
+    form_class    = StaffSignUpForm
+    template_name = 'users/account/signup_staff.html'
+    success_url   = reverse_lazy('login')
+
 
 # 登入
 class LoginView(auth_views.LoginView):
-    template_name = 'users/login.html'
+    template_name = 'users/account/login.html'
 
 # 登出
 class LogoutView(auth_views.LogoutView):
-    template_name = 'users/logged_out.html'
+    template_name = 'users/account/logged_out.html'
 
 # 密碼變更
 class PasswordChangeView(auth_views.PasswordChangeView):
-    template_name = 'users/password_change_form.html'
+    template_name = 'users/account/password_change_form.html'
     success_url   = reverse_lazy('password_change_done')
 
 class PasswordChangeDoneView(auth_views.PasswordChangeDoneView):
-    template_name = 'users/password_change_done.html'
+    template_name = 'users/account/password_change_done.html'
 
 # 密碼重設：輸入 email
 class PasswordResetView(auth_views.PasswordResetView):
-    template_name      = 'users/password_reset_form.html'
-    email_template_name = 'users/password_reset_email.html'
-    success_url        = reverse_lazy('password_reset_done')
+    template_name       = 'users/account/password_reset_form.html'
+    email_template_name = 'users/account/password_reset_email.html'
+    success_url         = reverse_lazy('password_reset_done')
 
 class PasswordResetDoneView(auth_views.PasswordResetDoneView):
-    template_name = 'users/password_reset_done.html'
+    template_name = 'users/account/password_reset_done.html'
 
 # 重設連結點開後：設定新密碼
 class PasswordResetConfirmView(auth_views.PasswordResetConfirmView):
-    template_name = 'users/password_reset_confirm.html'
+    template_name = 'users/account/password_reset_confirm.html'
     success_url   = reverse_lazy('password_reset_complete')
 
 class PasswordResetCompleteView(auth_views.PasswordResetCompleteView):
-    template_name = 'users/password_reset_complete.html'
+    template_name = 'users/account/password_reset_complete.html'
