a8e5650846d5112fc7f9ad198896936d84933c0d
WuTim901005 <tim901005.mg13@nycu.edu.tw>
2025-05-28 16:15:01 +0800

    新增中英文選項

--- CloudNative_final/urls.py
+++ CloudNative_final/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 from django.contrib.auth import views as auth_views
+from django.conf.urls.i18n import i18n_patterns
+from django.views.i18n import set_language
 
 # Import your custom views
 from users.views import (
@@ -23,4 +25,5 @@ urlpatterns = [
     path('orders/', include('orders.urls')),
     path('reviews/', include('reviews.urls')),
     
+    path('i18n/setlang/', set_language, name='set_language'),
 ]
