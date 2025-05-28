25f782c005b1ff63ad3674b6408ee5fbcb0440d1
顏于斌 <yubin@yanyubindeMacBook-Pro.local>
2025-05-28 14:13:45 +0800

    User

--- users/views.py
+++ users/views.py
@@ -25,17 +25,17 @@ class LoginView(auth_views.LoginView):
         messages.success(self.request, f"歡迎回來，{form.get_user().username}！")
         return super().form_valid(form)
 
-class LogoutView(auth_views.LogoutView):
-    # 允許 GET/POST
-    http_method_names = ['get', 'post', 'head', 'options']
-    # 登出後重定向到首頁
-    next_page = '/'  # 改為根路徑，即首頁
+class LogoutView(View):
+    """自定義登出視圖，直接登出並重定向"""
     
-    def dispatch(self, request, *args, **kwargs):
+    def get(self, request):
         if request.user.is_authenticated:
             messages.success(request, "您已成功登出。")
-            logout(request)  # 顯式調用登出功能
-        return super().dispatch(request, *args, **kwargs)
+            logout(request)
+        return redirect('/')
+    
+    def post(self, request):
+        return self.get(request)
 
 
 # 密碼變更
