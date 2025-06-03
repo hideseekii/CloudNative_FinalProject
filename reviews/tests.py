8e12860b572e12eb53949dfe86c27035dfee52cb
Yi-Chen-Yang <126686414+Yi-Chen-Yang@users.noreply.github.com>
2025-06-04 00:26:24 +0800

    Update tests.py

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from orders.models import Order, OrderItem
